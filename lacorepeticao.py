5c398594e6ea3f715a6d92729e34033ca36cf6d2
Faelreidelas <darimenezes7@gmail.com>
2026-05-21 15:54:51 -0300

    ok

---
+++ lacorepeticao.py
@@ -0,0 +1,8 @@
+alunos = ['Alice', 'Bob', 'Charlie', 'David']
+alunos.sort()
+print (f"quantos alunos tem na sala: {len(alunos)}")
+
+print("iniciando chamada de alunos: ")
+for aluno in alunos:
+    print (f"chamando aluno: {aluno}")  
+    print (f"{aluno} presente!")
